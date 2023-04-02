e334a281f3c6aeef0d1654db8da68b5a9470b813
RomanZakrenychnyi <116659319+RomanZakrenychnyi@users.noreply.github.com>
2023-04-02 12:28:41 +0300

    use hook useState, code become shorter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,54 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { FeedbackOption } from './FeedbackOption/FeedbackOption';
 import { Section } from './SectionTitle/SectionTitle';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleUpdate = e => {
+  const total = good + neutral + bad;
+  const percentage = Math.round((good / total) * 100);
+
+  const handleUpdate = e => {
     const { name } = e.currentTarget;
     switch (name) {
       case 'good':
-        this.setState(prevState => ({
-          good: prevState.good + 1,
-        }));
+        setGood(good + 1);
         break;
 
       case 'neutral':
-        this.setState(prevState => ({
-          neutral: prevState.neutral + 1,
-        }));
+        setNeutral(neutral + 1);
         break;
       case 'bad':
-        this.setState(prevState => ({
-          bad: prevState.bad + 1,
-        }));
+        setBad(bad + 1);
         break;
       default:
         throw new Error("Name doesn't exist");
     }
   };
 
-  calcTotal = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOption onLeaveFeedback={handleUpdate} />
+      </Section>
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.calcTotal();
-    const percentage = ((good / total) * 100).toFixed(0);
-
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOption onLeaveFeedback={this.handleUpdate} />
-        </Section>
-
-        <Section title="Statistics">
-          {total ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentag={percentage}
-            />
-          ) : (
-              <Notification message={'There is no feedback'} />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+      <Section title="Statistics">
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentag={percentage}
+          />
+        ) : (
+          <Notification message={'There is no feedback'} />
+        )}
+      </Section>
+    </>
+  );
+};
